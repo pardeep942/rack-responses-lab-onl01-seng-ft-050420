b32f303fb454e60825f1321fa160879990d1f8b2
PARDEEP KAUR <pkaur84@aol.com>
2020-06-15 16:32:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,17 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
+    ite "Good Afternoon!"
     end
- 
+
     resp.finish
   end
- 
+
 end
